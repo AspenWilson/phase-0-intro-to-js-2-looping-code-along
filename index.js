aff287b2c871404e8b5599d8df3a32474eac24e3
Aspen Wilson <aspen.schell@gmail.com>
2023-01-15 16:56:00 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, msg) {
+    const note= []
+    for (let i=0;i<names.length;i++) {
+        const message =`Thank you, ${names[i]}, for the wonderful ${msg} gift!`;
+        note.push(message)
+    }
+    return note;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],'surprise');
+
+function countDown(){
+    let countDown =10;
+    while (countDown>=0) {
+        console.log(countDown--);
+    }
+}
+
